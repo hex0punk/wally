a21136b2a9a28e21bb34ea69aa942855768f9696
hex0punk <alex.useche@trailofbits.com>
2024-01-26 15:41:06 -0800

    fixes to cosmograph. Upgraded to parcel 2.0

--- client/index.ts
+++ client/index.ts
@@ -1,332 +1,94 @@
-import cytoscape from 'cytoscape';
+import { Cosmograph } from '@cosmograph/cosmograph'
 import jsonData from "./nomad-json.json";
 
+// Types
+type Node = {
+    id: string;
+    color: string;
+    label: string;
+    x?: number;
+    y?: number;
+  }
+  
+  type Link = {
+    source: string;
+    target: string;
+  }
+
+
+// Load data from json
 const data: any = jsonData;
 
-let nodes: any[] = [];
-let edges: any[] = [];
+let nodes: Node[] = [];
+let links: Link[] = [];
 
-// data.findings.forEach((finding: any) => {
-//     finding.Paths.forEach((paths: string[]) => {
-//         let prev = "";
-//         paths.forEach((path, i) => {
-//             if (i === 0) {
-//                 prev = path;
-//                 addBaseNode(path);
-//             } else {
-//                 addNode(path);
-//                 addEdge(path, prev);
-//                 prev = path;
-//             }
-//         });
-//     });
-// });
-
-// if (data.findings != undefined) {
-    data.findings.forEach((finding: any) => {
-        // console.log(finding)
-        finding.Paths.forEach((paths: string | any[]) => {
-            let prev = "";
-            for (let i = 0; i < paths.length; i++) {
-                if (i == 0) {
-                    prev = paths[i]
-                    addBaseNode(paths[i])
-                } else {
-                    addNode(paths[i])
-                    addEdge(paths[i], prev)
-                    prev = paths[i]  
-                }  
+data.findings.forEach((finding: any) => {
+    finding.Paths.forEach((paths: string[]) => {
+        let prev = "";
+        paths.forEach((path, i) => {
+            if (i === 0) {
+                prev = path;
+                addNodeIfNotExist(path, "#984040");
+            } else {
+                addNodeIfNotExist(path, "#4287f5");
+                addEdgeIfNotExist(path, prev);
+                prev = path;
             }
-        })
+        });
     });
-// }
-
-function addNode(node: string) {
-    nodes.push({data: { id: node }, style: {'background-color': 'red', 'label': node }});
-    // graph.addNode(node, {label: node });
-}
-
-function addBaseNode(node: string) {
-    nodes.push({data: { id: node }, style: {'background-color': 'blue', 'label': node}});
-    // graph.addNode(node, {label: node });
-}
-
-function addEdge(nodeFrom: string, nodeTo: string) {
-    edges.push({data: {id: nodeFrom + "->" + nodeTo, source: nodeFrom, target: nodeTo}})
-    // if (!graph.hasEdge(nodeFrom, nodeTo)) {
-    //     graph.addEdge(nodeFrom, nodeTo);
-    // }
-}
-
-
-
-var cy = cytoscape({
-    container: document.getElementById('sigma-container'), // container to render in
-
-    elements: { // list of graph elements to start with
-        nodes: nodes,
-        edges: edges
-    },
-
-    boxSelectionEnabled: false,
-  
-    style: cytoscape.stylesheet()
-    //   .selector('node')
-    //     .style({
-    //       'content': 'data(id)'
-    //     })
-      .selector('edge')
-        .style({
-          'curve-style': 'bezier',
-          'target-arrow-shape': 'triangle',
-          'width': 4,
-          'line-color': '#ddd',
-          'target-arrow-color': '#ddd'
-        }),
-    //   .selector('.highlighted')
-    //     .style({
-    //       'background-color': '#61bffc',
-    //       'line-color': '#61bffc',
-    //       'target-arrow-color': '#61bffc',
-    //       'transition-property': 'background-color, line-color, target-arrow-color',
-    //       'transition-duration': '0.5s'
-    //     }),
-
-  layout: {
-    name: 'breadthfirst',
-    directed: true,
-    padding: 10,
-    fit: true,
-    spacingFactor: 1.0,
-    nodeDimensionsIncludeLabels: true
-  }
 });
 
+function nodeExists(nodeId: string): boolean {
+    return nodes.some(node => node.id === nodeId);
+}
 
-// cy.nodes('[id="N2"]').style('background-color', 'red');
+function edgeExists(source: string, target: string): boolean {
+    return links.some(link => link.source === source && link.target === target);
+}
 
-// // if (data.findings != undefined) {
-//     // data.findings.forEach((finding: any) => {
-//     //     // console.log(finding)
-//     //     finding.Paths.forEach((paths: string | any[]) => {
-//     //         let prev = "";
-//     //         for (let i = 0; i < paths.length; i++) {
-//     //             if (i == 0) {
-//     //                 prev = paths[i]
-//     //                 addNode(paths[i])
-//     //             } else {
-//     //                 addNode(paths[i])
-//     //                 addEdge(paths[i], prev)
-//     //                 prev = paths[i]  
-//     //             }  
-//     //         }
-//     //     })
-//     // });
-// // }
+function addNodeIfNotExist(nodeId: string, color: string) {
+    if (!nodeExists(nodeId)) {
+        let label = extractFuncFromId(nodeId)
+        label = label != null ? label : ""
+        nodes.push({ id: nodeId, label: label, color: color });
+    }
+}
 
-// import Sigma from "sigma";
-// import Graph from "graphology";
-// import circular from "graphology-layout/circular";
-// import forceAtlas2 from "graphology-layout-forceatlas2";
-// import { Coordinates, NodeDisplayData, EdgeDisplayData } from "sigma/types";
+function addEdgeIfNotExist(source: string, target: string) {
+    if (!edgeExists(source, target)) {
+        links.push({ source, target });
+    }
+}
 
-// import jsonData from "./nomad-json.json";
+function extractFuncFromId(nodeId: string): string | null {
+    const match = nodeId.match(/\[(.*?)\]/);
+    return match ? match[1] : null;
+}
 
-// const data: any = jsonData;
+// const canvas = document.getElementById("container")
 
-// // Retrieve some useful DOM elements:
-// const container = document.getElementById("sigma-container") as HTMLElement;
-// const searchInput = document.getElementById("search-input") as HTMLInputElement;
-// const searchSuggestions = document.getElementById("suggestions") as HTMLDataListElement;
+const canvas = document.createElement('div')
+document.body.appendChild(canvas)
 
-// // Instantiate sigma:
-// const graph = new Graph();
+const config = {
+    // backgroundColor: "#151515",
+    nodeSize: 3,
+    nodeColor: (n, i) => n.color,
+    linkWidth: 0.5,
+    linkColor: "#8C8C8C",
+    linkArrows: true,
+    nodeLabelAccessor: n => n.label,
+    simulation: {
+      repulsion: 0.5,
+    },
+    // renderLinks: true,
+    events: {
+      onClick: node => { console.log('Clicked node: ', node) },
+    },
+  }
+  
 
-// data.findings.forEach((finding: any) => {
-//     finding.Paths.forEach((paths: string[]) => {
-//         let prev = "";
-//         paths.forEach((path, i) => {
-//             if (i === 0) {
-//                 prev = path;
-//                 addNode(path);
-//             } else {
-//                 addNode(path);
-//                 addEdge(path, prev);
-//                 prev = path;
-//             }
-//         });
-//     });
-// });
+const cosmograph = new Cosmograph(canvas)
+cosmograph.setConfig(config)
 
-// function addNode(node: string) {
-//     if (!graph.hasNode(node)) {
-//         graph.addNode(node, {label: node });
-//     }
-// }
-
-// function addEdge(nodeFrom: string, nodeTo: string) {
-//     if (!graph.hasEdge(nodeFrom, nodeTo)) {
-//         graph.addEdge(nodeFrom, nodeTo);
-//     }
-// }
-
-// circular.assign(graph);
-// const settings = forceAtlas2.inferSettings(graph);
-// forceAtlas2.assign(graph, { settings, iterations: 600 });
-
-
-// // Type and declare internal state:
-// interface State {
-//   hoveredNode?: string;
-//   searchQuery: string;
-//   clickedNode?: string;
-//   connectedEdges?: Set<string>;
-
-//   // State derived from query:
-//   selectedNode?: string;
-//   suggestions?: Set<string>;
-
-//   // State derived from hovered node:
-//   hoveredNeighbors?: Set<string>;
-// }
-
-// const state: State = { searchQuery: "" };
-
-// // Feed the datalist autocomplete values:
-// searchSuggestions.innerHTML = graph
-//   .nodes()
-//   .map((node) => `<option value="${graph.getNodeAttribute(node, "label")}"></option>`)
-//   .join("\n");
-
-// const renderer = new Sigma(graph, container);
-
-// renderer.on("clickNode", ({ node }) => {
-//     console.log("called")
-//     // setClickedNode(node);
-// });
-
-
-// // Actions:
-// function setSearchQuery(query: string) {
-//   state.searchQuery = query;
-
-//   if (searchInput.value !== query) searchInput.value = query;
-
-//   if (query) {
-//     const lcQuery = query.toLowerCase();
-//     const suggestions = graph
-//       .nodes()
-//       .map((n) => ({ id: n, label: graph.getNodeAttribute(n, "label") as string }))
-//       .filter(({ label }) => label.toLowerCase().includes(lcQuery));
-
-//     if (suggestions.length === 1 && suggestions[0].label === query) {
-//       state.selectedNode = suggestions[0].id;
-//       state.suggestions = undefined;
-
-//       const nodePosition = renderer.getNodeDisplayData(state.selectedNode) as Coordinates;
-//       renderer.getCamera().animate(nodePosition, {
-//         duration: 500,
-//       });
-//     } else {
-//       state.selectedNode = undefined;
-//       state.suggestions = new Set(suggestions.map(({ id }) => id));
-//     }
-//   } else {
-//     state.selectedNode = undefined;
-//     state.suggestions = undefined;
-//   }
-
-//   renderer.refresh();
-// }
-
-// function setHoveredNode(node?: string) {
-//   if (node) {
-//     state.hoveredNode = node;
-//     state.hoveredNeighbors = new Set(graph.neighbors(node));
-//   } else {
-//     state.hoveredNode = undefined;
-//     state.hoveredNeighbors = undefined;
-//   }
-
-//   renderer.refresh();
-// }
-
-// function setClickedNode(node?: string) {
-//   if (node) {
-//     state.clickedNode = node;
-//     const connectedEdges = graph.edges(node);
-//     state.connectedEdges = new Set(connectedEdges);
-//   } else {
-//     state.clickedNode = undefined;
-//     state.connectedEdges = undefined;
-//   }
-
-//   renderer.refresh();
-// }
-
-// // Bind search input and graph interactions:
-// searchInput.addEventListener("input", () => {
-//   setSearchQuery(searchInput.value || "");
-// });
-// searchInput.addEventListener("blur", () => {
-//   setSearchQuery("");
-// });
-
-// renderer.on("enterNode", ({ node }) => {
-//   setHoveredNode(node);
-// });
-// renderer.on("leaveNode", () => {
-//   setHoveredNode(undefined);
-// });
-// renderer.on("clickNode", ({ node }) => {
-//     console.log("called")
-//   setClickedNode(node);
-// });
-
-// // Render nodes and edges accordingly to the internal state:
-// renderer.setSetting("nodeReducer", (node, data) => {
-//   const res: Partial<NodeDisplayData> = { ...data };
-
-//   if (state.hoveredNode) {
-//     if (state.hoveredNode !== node && !state.hoveredNeighbors?.has(node)) {
-//       res.label = "";
-//       res.color = "#f6f6f6";
-//     }
-//   }
-
-//   if (state.clickedNode) {
-//     if (state.clickedNode === node || (state.connectedEdges && Array.from(state.connectedEdges).some(edge => graph.hasExtremity(edge, node)))) {
-//       res.color = "#ff0000";
-//     } else {
-//       res.label = "";
-//       res.color = "#f6f6f6";
-//     }
-//   }
-
-//   if (state.selectedNode === node) {
-//     res.highlighted = true;
-//   } else if (state.suggestions && !state.suggestions.has(node)) {
-//     res.label = "";
-//     res.color = "#f6f6f6";
-//   }
-
-//   return res;
-// });
-
-// renderer.setSetting("edgeReducer", (edge, data) => {
-//   const res: Partial<EdgeDisplayData> = { ...data };
-
-//   if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
-//     res.hidden = true;
-//   }
-
-//   if (state.clickedNode && (!state.connectedEdges?.has(edge))) {
-//     res.hidden = true;
-//   }
-
-//   if (state.suggestions && (!state.suggestions.has(graph.source(edge)) || !state.suggestions.has(graph.target(edge)))) {
-//     res.hidden = true;
-//   }
-
-//   return res;
-// });
+cosmograph.setData(nodes, links)
